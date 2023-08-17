5578eb81b8338f6809bde3fc7c076ed9c0577a70
rbonUniverse <101582640+rbonUniverse@users.noreply.github.com>
2023-08-17 10:35:59 +0300

    Update database.js
    Changed the local MongoDB database link to MongoDB Atlas claud link

--- TV Series and Subscription-management/APIServer/configs/database.js
+++ TV Series and Subscription-management/APIServer/configs/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // MongoDB connection path
 const connectToDataBase = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/subscriptionsDB");
+  await mongoose.connect("mongodb+srv://robbyzigi:nUD1ZDyGX2O5SaOD@cluster0.zawahrr.mongodb.net/usersDB");
   console.log("Connected to Mongo");
 };
 
